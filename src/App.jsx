fcc1df7cca1aff21926ee88ed803d6cb9e596d5f
Nousad <nousadaliinps@gmail.com>
2024-07-24 14:44:34 +0530

    Create Vite File Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+console.log(process.env.REACT_APP_APPWRITE_URL)
+  return (
+    <>
+      A Blog With AppWrite
+    </>
+  )
+}
+
+export default App
